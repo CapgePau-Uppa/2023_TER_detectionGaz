c9bd081471d83ee21ff55035469f0223d8929cd5
Kraosh <114225492+Kraosh@users.noreply.github.com>
2023-02-01 19:34:04 +0100

    lecture capteur

--- Gas_Sensor/Gas_Sensor.ino
+++ Gas_Sensor/Gas_Sensor.ino
@@ -1,9 +1,16 @@
 
+int analog_IN = A0;  // This is our input pin
+
 void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(analog_IN, INPUT);
+  Serial.begin(9600);
 }
 
 void loop() {
-  
+  int Value = analogRead(analog_IN);  
+  Serial.print("Analog read: ");
+  Serial.println(Value);  
+
   delay(1000);                       // wait for a second
 }

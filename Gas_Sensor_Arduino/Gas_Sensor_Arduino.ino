b89c54aa33d00e6f3d76e2a45bc78b757cf5d33b
Kraosh <114225492+Kraosh@users.noreply.github.com>
2023-03-08 16:31:55 +0100

    changed proof of concept & updated slideshow

--- Gas_Sensor_Arduino/Gas_Sensor_Arduino.ino
+++ Gas_Sensor_Arduino/Gas_Sensor_Arduino.ino
@@ -1,32 +1,25 @@
 #include <MQ2.h>
 
-int analog_IN = A0;  // This is our input pin
-int digital_IN = D1; // This is our input pin
+int analog_IN = D0;  // This is our input pin (IO2)
 MQ2 mq2(analog_IN);
 
-void setup()
-{
-  // pinMode(LED_BUILTIN, OUTPUT);
-  // pinMode(analog_IN, INPUT);
+void setup() {
   Serial.begin(9600);
-
+  
   mq2.begin();
+  delay(20000);
 }
 
-void loop()
-{
+void loop() {
+  
+  
+  float* values= mq2.read(true);
+  float smokeLvl= mq2.readSmoke(); 
+  Serial.print("MQ2 Read SMOKE : "); Serial.println(smokeLvl); 
 
-  float *values = mq2.read(true);
-  Serial.print("MQ2 Read CO : ");
-  Serial.println(mq2.readCO());
-  Serial.print("Digital Read : ");
-  Serial.println(digitalRead(digital_IN));
-  Serial.print("Analogic Read : ");
-  Serial.println(analogRead(analog_IN));
-  /*if (digitalRead(digital_IN) == LOW){
-    Serial.println("Seuil dépassé : Gaz detecté");
-  }*/
-
-  Serial.println();
-  delay(1000); // wait for a second
+  if (smokeLvl > 0.03){
+    Serial.println("Seuil dépassé : CO2 détecté");
+  }
+  
+  delay(1000);                       // wait for a second
 }

524da41c894da7ebc49d0f45a43f4c743e134730
SamuelACGarcia <samuel.ac.garcia@gmail.com>
2023-03-23 11:55:10 +0100

    🚧 AlertFragment created

--- Gaz_Detector_App/New Java/GazDetector/app/src/main/java/com/arangarcia/gazdetector/ui/alert/AlertFragment.java
+++ Gaz_Detector_App/New Java/GazDetector/app/src/main/java/com/arangarcia/gazdetector/ui/alert/AlertFragment.java
@@ -7,8 +7,10 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.example.gazdetector.R;
+import com.example.gazdetector.databinding.FragmentPlanBinding;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -16,6 +18,8 @@ import com.example.gazdetector.R;
  * create an instance of this fragment.
  */
 public class AlertFragment extends Fragment {
+    private com.ortiz.touchview.TouchImageView imageViewPlan;
+    private TextView posTextView;
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -55,12 +59,28 @@ public class AlertFragment extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+        FragmentPlanBinding binding = FragmentPlanBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+        imageViewPlan = root.findViewById(R.id.imageViewPlan);
+        posTextView = root.findViewById(R.id.posTextView);
+
+        imageViewPlan.setOnTouchImageViewListener(new com.ortiz.touchview.TouchImageView.OnTouchImageViewListener () {
+            @Override
+            public void onMove() {
+
+                posTextView.setText(imageViewPlan.getZoomedRect().toString());
+                //Log.d("Samuel_Plan","J'ai touché");
+            }
+        });
+
         return inflater.inflate(R.layout.fragment_alert, container, false);
     }
 }

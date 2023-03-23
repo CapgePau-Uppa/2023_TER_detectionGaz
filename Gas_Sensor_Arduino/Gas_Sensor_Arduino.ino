16a3181fb43fd1a204911780c58f0d07c5a6df85
Kraosh <114225492+Kraosh@users.noreply.github.com>
2023-03-23 14:37:50 +0100

    ✨Added automatic reset on arduino

--- Gas_Sensor_Arduino/Gas_Sensor_Arduino.ino
+++ Gas_Sensor_Arduino/Gas_Sensor_Arduino.ino
@@ -19,11 +19,20 @@ void loop()
 
   float *values = mq2.read(false);
   float smokeLvl = mq2.readSmoke()*1000;
-  /*if(smokeLvl == NAN  || smokeLvl == INFINITY){
+  if(smokeLvl == NAN  || smokeLvl == INFINITY){
     Serial.println("inf");
-  }*/
+    resetFunc();
+  }
   float LPGLvl = mq2.readLPG()*1000;
+  if(LPGLvl == NAN  || LPGLvl == INFINITY){
+    Serial.println("inf");
+    resetFunc();
+  }
   float COLvl = mq2.readCO()*1000;
+  if(COLvl == NAN  || COLvl == INFINITY){
+    Serial.println("inf");
+    resetFunc();
+  }
   Serial.print("{\"smoke\":");
   Serial.print(smokeLvl);
   Serial.print(",\"LPG\":");

27bbe3e339eb305778b6881356958d2e3def9482
Kraosh <114225492+Kraosh@users.noreply.github.com>
2023-03-30 12:45:22 +0200

    working on server

--- Gas_Sensor_Server/server.js
+++ Gas_Sensor_Server/server.js
@@ -55,8 +55,10 @@ mongoClient.connect(url, (err, db) => {
             } );*/
             
         });
+
+        
+        app.listen(3000, () => {
+            console.log("listening on port 3000");
+        })
     }
-})
-app.listen(3000, () => {
-    console.log("listening on port 3000");
 })
